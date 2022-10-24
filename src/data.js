12c2a9d0d85034180653e0de61b975a7110e19bc
Keixys <reyeskeixysmar@gmail.com>
2022-10-24 13:22:01 -0500

    los problemas de los filtros

--- src/data.js
+++ src/data.js
@@ -1,9 +1,29 @@
+// import marco from "./mainpeliculas";
+
+//const datalover={
+ 
+  //import data from './data/ghibli/ghibli.js';
+
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const movie = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+export const filtrodefechas = (datapelicula,fecha) => {
+datapelicula.filter(movie => {
+  if (movie.release_date === fecha) { 
+      return movie;
+    }
+  
+})
+
+}
+
+;
+// }
+// export default datalover;
--- src/mainpeliculas.js
+++ src/mainpeliculas.js
@@ -1,9 +1,17 @@
 import data from './data/ghibli/ghibli.js';
+import { filtrodefechas } from './data.js';
+//import datalover from './data.js';
+// import movie from "./data.js"
 
 const contenido = document.querySelector("#lista")
 const fragment = document.createDocumentFragment()
 
-data.films.forEach(film => {
+export const todoslosdatos= data.films;
+
+let filtros = filtrodefechas (todoslosdatos,"2008");
+console.log(filtros)
+
+ todoslosdatos.forEach(film => {
     const titulo = document.createElement("p")
     titulo.textContent = film.title;
     const imagen = document.createElement("img")
@@ -19,6 +27,9 @@ data.films.forEach(film => {
     marco.appendChild(pelicula)
     marco.classList.add("marco")
     fragment.appendChild(marco)
+
+     console.log(filtrodefechas)
+    
 })
 
 contenido.appendChild(fragment)
