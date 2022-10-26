7095966fb4ccd431781ba5910e78f53f960c8674
Gabriela Wongchuig <gaba@MacBook-Air-de-Gabriela.local>
2022-10-26 00:08:08 -0500

    hace filtros y tiene el boton de reset

--- src/mainpersonajes.js
+++ src/mainpersonajes.js
@@ -1,22 +1,48 @@
 import data from './data/ghibli/ghibli.js';
-
-
-
+import { filtrodepersonaje } from './data.js';
 
 const contenido = document.querySelector("#listapersonajes")
 const fragment = document.createDocumentFragment()
 
+const todoslospersonajes = data.films;
 
+const filtropersonaje = document.getElementById("filtropersonaje")
+filtropersonaje.addEventListener("change", (event) => {
+const fpersonajes = filtrodepersonaje (todoslospersonajes, event.target.value);
 
-data.films.forEach(film => {  
-
+contenido.innerHTML=""
+fpersonajes.forEach(film => {  
     film.people.forEach(personaje => {
     const titulo = document.createElement("p")
     titulo.textContent = personaje.name;
     const imagen = document.createElement("img")
     imagen.src = personaje.img;
     
+    const persona = document.createElement("div")
+    persona.appendChild(imagen)
+    persona.appendChild(titulo)
 
+    const marco = document.createElement("div")
+    marco.appendChild(persona)
+    marco.classList.add("marco")
+    fragment.appendChild(marco)
+  
+     //   console.log(filtrodefechas)
+      
+  })
+  
+  contenido.appendChild(fragment)
+
+})
+})
+
+todoslospersonajes.forEach(film => {  
+    film.people.forEach(personaje => {
+    const titulo = document.createElement("p")
+    titulo.textContent = personaje.name;
+    const imagen = document.createElement("img")
+    imagen.src = personaje.img;
+    
     const persona = document.createElement("div")
     persona.appendChild(imagen)
     persona.appendChild(titulo)
@@ -26,8 +52,16 @@ data.films.forEach(film => {
     marco.classList.add("marco")
     fragment.appendChild(marco)
 })
-
+contenido.appendChild(fragment)
 })
 
 
-contenido.appendChild(fragment)
+
+// todoslospersonajes.forEach(title => {
+// title.innerHTML= document.getElementById ("personajes")
+// const html = "<option> `${ title }` </option>"
+// title.insertAdjacentHTML("", html)
+// console.log ("title", title)
+
+
+
