38dbed19efa42f2863896dc1a373354767c1cb9a
Gabriela Wongchuig <gaba@MacBook-Air-de-Gabriela.local>
2022-10-26 04:17:53 -0500

    actualiza el css y lo hace responsive

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import data from './data/ghibli/ghibli.js';
 // for(let i = 0; i<data.films.length; i++){
 // console.log(data.films[i].title)
 // }
+const ham = document.querySelector(".ham");
+const enlaces = document.querySelector(".enlace");
+// se usa el all porque estamos diciendo que coja todas las barras
+const barras= document.querySelectorAll(".ham span");
+
+
+ham.addEventListener("click", () =>{
+    enlaces.classList.toggle("activado");
+    barras.forEach(child=>{child.classList.
+    toggle(`animado`)});
+  // para cada elemento de la variable barras que incluye los 3 elementos span le agregamos la clase animado 
+})//toggle se usa para alternar, que va a cambiar al displey none a display flex
 
 console.log(data)
 
