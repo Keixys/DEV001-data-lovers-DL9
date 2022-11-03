08bf31e8834be9a43dcc67973c3b3847947d12ef
Keixys <reyeskeixysmar@gmail.com>
2022-11-02 21:44:31 -0500

    sube los cambios del miercoles

--- src/data.js
+++ src/data.js
@@ -1,18 +1,3 @@
-// import marco from "./mainpeliculas";
-
-//const datalover={
-
-//import data from './data/ghibli/ghibli.js';
-
-// estas funciones son de ejemplo
-
-// export const movie = () => {
-//   return 'example';
-// };
-
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
 
 export const filtrodedirector = (datapelicula, director) => {
   return datapelicula.filter(cadadirector => {
@@ -26,8 +11,6 @@ export const filtrodedirector = (datapelicula, director) => {
 }
 export const filtrodepersonaje = (datapelicula, nombredepelicula) => {
   return datapelicula.filter(cadatitulo => {
-    // console.log("title", cadatitulo.title)
-    // console.log("title", cadatitulo)
     if (cadatitulo.title === nombredepelicula) {
       return cadatitulo;
     }
@@ -35,83 +18,78 @@ export const filtrodepersonaje = (datapelicula, nombredepelicula) => {
   )
 }
 
-
-export const filtrodegenero = (datadepelicula, nombreDeGenero)=> {
-  const todoslospersonajes =[]
+export const filtrodegenero = (datadepelicula, nombreDeGenero) => {
+  const todoslospersonajes = []
   datadepelicula.forEach(film => {
-    const personajes = film.people.filter(personaje =>{
-      if(personaje.gender === nombreDeGenero){
+    const personajes = film.people.filter(personaje => {
+      if (personaje.gender === nombreDeGenero) {
         return personaje;
       }
     })
     todoslospersonajes.push(personajes)
   });
+  console.log(todoslospersonajes.flat())
   return todoslospersonajes.flat()
+
 }
 
-export const filtrodeespecie = (dataDePersonaje, nombreDeEspecie) => {
-  const todoslospersonajes =[]
-  dataDePersonaje.forEach(film => {
-    const especie = film.people.filter (especie =>{
-      if(especie.specie === nombreDeEspecie){
-
+export const filtrodeespecie = (dataDePelicula, nombreDeEspecie) => {
+  const todoslospersonajes = []
+  dataDePelicula.forEach(film => {
+    const especie = film.people.filter(especie => {
+      if (especie.specie === nombreDeEspecie) {
         return especie;
       }
     })
     todoslospersonajes.push(especie)
+    // console.log(todoslospersonajes.push())
   });
+  console.log(todoslospersonajes.flat())
   return todoslospersonajes.flat()
+
 }
 
 
 export const ordenAyZ = (datadepelicula) => {
-  const ordendepersonajes =[]
-  datadepelicula.forEach(film=> {
-   const resultado = film.people.sort((a, b) =>{
-     if (a.name < b.name) {
-       return -1
-     }
-     if (a.name > b.name) {
-       return 1
-     }
-     if(a.name === b.name ){
-     return 0 
-    }
-return resultado
-   })
-  ordendepersonajes.push(resultado)
-   console.log (resultado)
- });
+  const ordendepersonajes = []
+  datadepelicula.forEach(film => {
+    const resultadouno = film.people.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1
+      }
+      if (a.name > b.name) {
+        return 1
+      }
+      if (a.name === b.name) {
+        return 0
+      }
+      return resultadouno
+    })
+    ordendepersonajes.push(resultadouno)
+  });
+  return ordendepersonajes.flat()
+}
+
+export const ordenZaA = (datadepelicula) => {
+  const ordendepersonajes = []
+  datadepelicula.forEach(film => {
+    const resultado = film.people.sort((a, b) => {
+      if (a.name < b.name) {
+        return 1
+      }
+      if (a.name > b.name) {
+        return -1
+      }
+      if (a.name === b.name) {
+        return 0
+      }
+      return resultado
+    })
+    ordendepersonajes.push(resultado)
+    
+  });
+  
   return ordendepersonajes.flat()
 
 }
-
-
-  // return datapersonaje.filter(cadaEspecie => {
-  //    console.log("specie", cadaEspecie.specie)
-  //    console.log("specie", cadaEspecie)
-  //   if (cadaEspecie.specie === nombredeespecie) {
-  //     return cadaEspecie;
-    
-//   datapelicula.forEach(film => {
-//     film.people.forEach(personaje => {
-//    personaje.filter(cadaGenero => {
-//       if (cadaGenero.gender == nombredegenero){
-//         return cadaGenero;
-//       }
-//     })
-//     });
-//   })
-
-// }
-
-//   return datapersonaje.filter(cadaGenero => {
-//     console.log("gender", cadaGenero.gender)
-//     console.log("gender", cadaGenero)
-//     if (cadaGenero.gender == nombredegenero){
-//       return cadaGenero;
-//     }
-//   }
-//   )
-// }
 
--- src/mainpersonajes.js
+++ src/mainpersonajes.js
@@ -1,5 +1,5 @@
 import data from './data/ghibli/ghibli.js';
-import { filtrodepersonaje, filtrodegenero, filtrodeespecie, ordenAyZ} from './data.js';
+import { filtrodepersonaje, filtrodegenero, filtrodeespecie, ordenAyZ, ordenZaA} from './data.js';
 
 const contenido = document.querySelector("#listapersonajes")
 const fragment = document.createDocumentFragment()
@@ -12,86 +12,76 @@ filtropersonaje.addEventListener("change", (event) => {
     //console.log (event.target)
     const fpersonajes = filtrodepersonaje(todaslaspeliculas, event.target.value);
     contenido.innerHTML = ""
-   // console.log (fpersonajes)
+    // console.log (fpersonajes)
     cartaFilms(fpersonajes)
 })
-// const resultado= filtrodegenero(data)
-// console.log (resultado)o
+
 const filtroGenero = document.querySelector("#filtroGenero")
-filtroGenero.addEventListener("change",(event)=>{
-    //console.log (event.target)
-     const fGenero = filtrodegenero (todaslaspeliculas,event.target.value);
-    console.log (fGenero)
-     contenido.innerHTML = ""
-     carta(fGenero)
+filtroGenero.addEventListener("change", (event) => {
+    const fGenero = filtrodegenero(todaslaspeliculas, event.target.value);
+    contenido.innerHTML = ""
+    carta(fGenero)
 })
 
 const filtroEspecie = document.querySelector("#filtroEspecie")
 filtroEspecie.addEventListener("change", (event) => {
     const fEspecie = filtrodeespecie(todaslaspeliculas, event.target.value);
     contenido.innerHTML = ""
-   carta(fEspecie)
-    
+    carta(fEspecie)
+
 })
 
-
-// const ordenarPer = document.querySelector("#ordenarPer")
-// ordenarPer.addEventListener("change", (event) => {
-//     const oPer= ordenAyZ (todaslaspeliculas, event.target.value);
-//     console.log(oPer)
-//     contenido.innerHTML= oPer
-   
-// })
-
-// const ordenarPer = document.querySelector("#ordenarPer")
-// ordenarPer.addEventListener ("change", (event) => {
-//     const oPer= ordenAyZ (todaslaspeliculas, event.target.value);
-//      contenido.innerHTML= ""
-//      cartaFilms(oPer)
-// })
 const ordenarPer = document.querySelector("#ordenarPer")
 ordenarPer.addEventListener("change", (event) => {
-    if(event.target.value === "A a la Z"){
-    const oPer= ordenAyZ (todaslaspeliculas, event.target.value)
-    contenido.innerHTML= ""
-    carta(oPer)
-   console.log(oPer)
-}
+    if (event.target.value === "A a la Z") {
+        const oPer = ordenAyZ(todaslaspeliculas, event.target.value)
+        contenido.innerHTML = ""
+        carta(oPer)
+       
+    }
+})
+const ordenarPerReverse = document.querySelector("#ordenarPer")
+ordenarPerReverse.addEventListener("change", (event) => {
+    if (event.target.value === "Z a la A") {
+        const oPer = ordenZaA(todaslaspeliculas, event.target.value)
+        contenido.innerHTML = ""
+        carta(oPer)
+        
+    }
 })
 
+
 function carta(todoslospersonajes) {
-        todoslospersonajes.forEach (personaje => {
-            const titulo = document.createElement("p")
-            titulo.textContent = personaje.name
-            //console.log(personaje)
-            // console.log(personaje.name);
-            const imagen = document.createElement("img")
-            imagen.src = personaje.img;
-            const genero = document.createElement("p")
-            genero.textContent = personaje.gender;
-            const especie = document.createElement("p")
-            especie.textContent = personaje.specie;
+    todoslospersonajes.forEach(personaje => {
+        const titulo = document.createElement("p")
+        titulo.textContent = personaje.name
+        const imagen = document.createElement("img")
+        imagen.src = personaje.img;
+        const genero = document.createElement("p")
+        genero.textContent = personaje.gender;
+        const especie = document.createElement("p")
+        especie.textContent = personaje.specie;
 
-            const persona = document.createElement("div")
-            persona.appendChild(imagen)
-            persona.appendChild(titulo)
-            persona.appendChild(genero)
-            persona.appendChild(especie)
+        const persona = document.createElement("div")
+        persona.appendChild(imagen)
+        persona.appendChild(titulo)
+        persona.appendChild(genero)
+        persona.appendChild(especie)
 
-            const marco = document.createElement("div")
-            marco.appendChild(persona)
-            marco.classList.add("marco")
-            fragment.appendChild(marco)
+        const marco = document.createElement("div")
+        marco.appendChild(persona)
+        marco.classList.add("marco")
+        fragment.appendChild(marco)
         // console.log(personaje.name)
-        })
-        contenido.appendChild(fragment)
-    
+    })
+    contenido.appendChild(fragment)
+
 }
 
 function cartaFilms(todoslospersonajes) {
     todoslospersonajes.forEach(film => {
         const personajes = film.people
-        personajes.forEach (personaje => {
+        personajes.forEach(personaje => {
             const titulo = document.createElement("p")
             titulo.textContent = personaje.name
             console.log(personaje)
